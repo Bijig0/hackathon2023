0b66afa971bb29f9836f6183a2ca29a95e244fe4
nitipat21 <nitipat.rstw@gmail.com>
2023-03-05 11:51:40 +1100

    Revert "save"
    This reverts commit c697abe00be82f55aed1079f675d64d8ca4074f3.

--- screens/MainApp/MainApp.tsx
+++ screens/MainApp/MainApp.tsx
@@ -1,29 +1,23 @@
-import { Text, View, FlatList, SectionList, StyleSheet } from 'react-native';
-import tw from '../.././lib/tailwind';
-import { useFonts } from 'expo-font';
-import React, { useCallback, useState, useContext, useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { Text, View, FlatList, SectionList, StyleSheet } from "react-native";
+import tw from "../.././lib/tailwind";
+import { useFonts } from "expo-font";
+import React, { useCallback, useState, useContext, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import {
   BottomTabNavigationProp,
   createBottomTabNavigator,
-} from '@react-navigation/bottom-tabs';
+} from "@react-navigation/bottom-tabs";
 // import useMainAppIsReady from "../../hooks/useAppIsReady";
-import * as SplashScreen from 'expo-splash-screen';
-import CameraScreen from './screens/Camera';
-import Home from './screens/Home';
-import Profile from './screens/Profile';
-import Footer from './Footer';
-import Login from '../Auth/screens/Login';
-import SignUpScreen from '../Auth/screens/SignUp';
-import SignUp from '../Auth/screens/SignUp';
-import ResetPassword from '../Auth/screens/ResetPassword';
-import ForgotPassword from '../Auth/screens/ForgotPassword';
-import { Welcome } from '../Auth/screens/Welcome';
+import * as SplashScreen from "expo-splash-screen";
+import CameraScreen from "./screens/Camera";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+import Footer from "./Footer";
 
 // SplashScreen.preventAutoHideAsync()
 
 export default function MainApp() {
-  console.log('main app');
+  console.log("main app");
   const Tab = createBottomTabNavigator();
 
   // const mainAppIsReady = useMainAppIsReady(data[0].data[0]);
@@ -50,11 +44,6 @@ export default function MainApp() {
       initialRouteName="Home"
     >
       <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Login" component={Login} />
-      <Tab.Screen name="Signup" component={SignUp} />
-      <Tab.Screen name="ResetPassword" component={ResetPassword} />
-      <Tab.Screen name="ForgotPassword" component={ForgotPassword} />
-      <Tab.Screen name="Welcome" component={Welcome} />
       <Tab.Screen
         name="CameraScreen"
         component={CameraScreen}
--- screens/MainApp/screens/Profile.tsx
+++ screens/MainApp/screens/Profile.tsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react'
 
-type Props = {};
+type Props = {}
 
 const Profile = () => {
-  return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  );
-};
 
-export default Profile;
+}
+
+export default Profile
